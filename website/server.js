4210e0bd7b418e93c7b24cf88381e972c5a04bee
programmer <omkarsk98@gmail.com>
2018-10-08 15:39:50 +0530

    Server ready

---
+++ website/server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 8000;
+const cors = require('cors');
+const logger = require('morgan');
+
+const index = require('./routes/indexRoutes');
+const todos = require('./routes/todosRoutes');
+
+const app = express();
+app.use(express.static(__dirname+'/frontend/build/'))
+
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+app.get('/*',function(req, res){
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
+app.use(logger('dev'));
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use('/', index);
+// app.use('/todos', todos);
+
+app.listen(port, function() {
+  console.log("listening on port: ", port);
+})
