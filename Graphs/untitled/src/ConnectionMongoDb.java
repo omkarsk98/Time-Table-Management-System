dbe86165437246f3fdd15057f2c0eebd06d7cea0
realArcherL <42532003+realArcherL@users.noreply.github.com>
2018-11-10 22:04:58 +0530

    Create ConnectionMongoDb.java

---
+++ Graphs/untitled/src/ConnectionMongoDb.java
@@ -0,0 +1,16 @@
+//import com.mongo.DB;
+//import com.mongodb.MongoClient;
+
+
+public class ConnectionMongoDb {
+    public static void main(String[] args){
+        try {
+            //MongoClient mongoClient = new MongoClient("localhost", 00000);
+            //DB db = mongoClient.getDB("Database name");
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
